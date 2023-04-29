1c67361fe9dd5eefb462e364f3d2d54c38bfc181
TalhaMaalik <48487598+TalhaMaalik@users.noreply.github.com>
2023-04-29 16:48:24 +0100

    Testcases for mapper completed

---
+++ TechnicalTaskShawbrook.Tests/CustomerProfileTest.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using BusinessLayer.Controllers;
+using BusinessLayer.DTOs;
+using BusinessLayer.Profiles;
+using DataAccessLayer.DataModel;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TechnicalTaskShawbrook.Tests
+{
+    public class CustomerProfileTest
+    {
+        private readonly IMapper _Mapper;
+
+        public CustomerProfileTest()
+        {
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomerProfile>();
+            });
+
+            _Mapper = mapperConfig.CreateMapper();
+        }
+
+
+        [Fact]
+        public void CreateMap_ShouldMapCustomerModel_ToCustomerReadDTO()
+        {
+
+            //Act
+            var result = _Mapper.Map<CustomerReadDTO>(new CustomerModel());
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void CreateMap_ShouldMapAllValuesCustomerModel_ToCustomerReadDTO()
+        {
+            //Arrange
+            var guid = Guid.NewGuid();
+            var customerModel = new CustomerModel() { Id = guid, Name = "Talha Malik", Email = "talha@test.com" };
+
+            //Act
+            var result = _Mapper.Map<CustomerReadDTO>(customerModel);
+
+            //Assert
+            Assert.Equal(customerModel.Id, result.Id);
+            Assert.Equal(customerModel.Name, result.Name);
+            Assert.Equal(customerModel.Email, result.Email);
+        }
+
+        [Fact]
+        public void CreateMap_ShouldNotMapCustomerModel_ToCustomerReadDTO_WhenPassedNull()
+        {
+
+            //Act
+            var result = _Mapper.Map<CustomerReadDTO>(null);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void CreateMap_ShouldMapCustomerCreateDTO_ToCustomerModel()
+        {
+
+            //Act
+            var result = _Mapper.Map<CustomerModel>(new CustomerCreateDTO());
+
+            // Assert
+            Assert.NotNull(result);
+        }
+        [Fact]
+        public void CreateMap_ShouldMapAllValuesCustomerCreateDTO_ToCustomerModel()
+        {
+            //Arrange
+            var customerDTO = new CustomerCreateDTO() { Name = "Talha Malik", Email = "talha@test.com" };
+
+            //Act
+            var result = _Mapper.Map<CustomerModel>(customerDTO);
+
+            //Assert
+            Assert.Equal(customerDTO.Name, result.Name);
+            Assert.Equal(customerDTO.Email, result.Email);
+        }
+        [Fact]
+        public void CreateMap_ShouldNotMapCustomerCreateDTO_ToCustomerModel_WhenPassedNull()
+        {
+
+            //Act
+            var result = _Mapper.Map<CustomerModel>(null);
+
+            // Assert
+            Assert.Null(result);
+        }
+    }
+}
---
+++ TechnicalTaskShawbrook.Tests/PurchaseOrderProfileTest.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using BusinessLayer.DTOs;
+using BusinessLayer.Models.Base;
+using BusinessLayer.Profiles;
+using DataAccessLayer.DataModel;
+using Xunit;
+
+namespace TechnicalTaskShawbrook.Tests
+{
+    public class PurchaseOrderProfileTest
+    {
+        private readonly IMapper _Mapper;
+
+        public PurchaseOrderProfileTest()
+        {
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<PurchaseOrderProfile>();
+            });
+
+            _Mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public void CreateMap_ShouldMapPurchaseOrderCreateDTO_ToPurchaseOrder()
+        {
+
+            //Act
+            var result = _Mapper.Map<PurchaseOrder>(new PurchaseOrderCreateDTO());
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void CreateMap_ShouldMapAllValuesPurchaseOrderCreateDTO_ToPurchaseOrder()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            var purchaseOrderDTO = new PurchaseOrderCreateDTO() { CustomerId = id };
+
+            //Act
+            var result = _Mapper.Map<PurchaseOrder>(purchaseOrderDTO);
+
+            //Assert
+            Assert.Equal(purchaseOrderDTO.CustomerId, result.CustomerId);
+        }
+
+        [Fact]
+        public void CreateMap_ShouldNotMapPurchaseOrderCreateDTO_ToPurchaseOrder_WhenValueIsNull()
+        {
+
+            //Act
+            var result = _Mapper.Map<PurchaseOrder>(null);
+
+            //Assert
+            Assert.Null(result);
+        }
+    }
+}
